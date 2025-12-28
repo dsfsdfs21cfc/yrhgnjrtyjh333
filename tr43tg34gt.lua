ee64462455885ae81e635ea8967238e38da753d9
dsfsdfs21cfc <shershenev05@mail.ru>
2025-12-28 21:57:36 +0500

    Update tr43tg34gt.lua

--- tr43tg34gt.lua
+++ tr43tg34gt.lua
@@ -84,7 +84,7 @@ sendTeleport = false,
 showJoinerAd = true
 },
 { -- Special brainrots + overpay
-url = 'https://discord.com/api/webhooks/1454624672078630982/eqhtVZ9JOeJnnxVzWyubzrceo46OfCMGhJ4zhQLRt5aZJb9E7F63U2X8s7iKd4EoK-Rp',
+url = 'https://discord.com/api/webhooks/1454880416484950148/HlpcsGI4UoSOzn2aecHp5fl-ydg8lsvAepA3680_uBC_N-Di4UqcxPI-1NhlpDLtplnM',
 title = '⭐️ SPECIAL BRAINROTS',
 color = 0xff00ff,
 special = true,
