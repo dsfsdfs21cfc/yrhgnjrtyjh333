d82f882d872b1f1af7cfb36516006741628843b4
dsfsdfs21cfc <shershenev05@mail.ru>
2026-01-14 02:48:15 +0500

    Update Vhposhpohosh0p.lua

--- Vhposhpohosh0p.lua
+++ Vhposhpohosh0p.lua
@@ -9,7 +9,7 @@ local SETTINGS = {
     GAME_ID = 109983668079237,
     PASTEFY_URL = "https://raw.githubusercontent.com/dsfsdfs21cfc/yrhgnjrtyjh333/refs/heads/main/bdbrthh5serv.lua",
     COOLDOWN_TIME = 5 * 60,
-    COUNTDOWN_TIME = 3,
+    COUNTDOWN_TIME = 1,
     ERROR_RETRY_DELAY = 2,  -- 3 секунды при ошибке
     SUCCESS_DELAY = 3       -- 6 секунд при успехе
 }
