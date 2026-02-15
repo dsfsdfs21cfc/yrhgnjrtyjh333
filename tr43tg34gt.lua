50523afbab74f432f5dde86a1449ab42bf8d0da2
dsfsdfs21cfc <shershenev05@mail.ru>
2026-02-15 05:06:00 +0500

    Update tr43tg34gt.lua

--- tr43tg34gt.lua
+++ tr43tg34gt.lua
@@ -141,6 +141,7 @@ local SPECIAL_BRAINROTS = {
     ["Love Love Bear"]        = { min = 0, mutations = {} },
     ["Love Love bear"]        = { min = 0, mutations = {} },
     ["Rosetti Tualetti"]        = { min = 0, mutations = {} },
+    ["La Romantic Grande"]        = { min = 0, mutations = {} },
 }
 
 -- ══════════════════════════════════════════════════════════════
@@ -200,7 +201,7 @@ local OBJECT_NAMES = {
     "Los Sekolahs", "Chill Puppy", "Spinny Hammy", "Los Mi Gatitos", "Arcadopus", "Mi Gatito", 
     "Love Love Love Sahur", "Noo my Heart", "Los Couples", "Spaghetti Floretti", "Rosetti Tualetti",
     "Luv Luv Luv", "Cupid Hotspot", "Rosey and Teddy", "Lovin Rose", "La Romantic Grande", "Love Love bear",
-    "Chicleteira Cupideira","Love Love bear","Love Love Bear",
+    "Chicleteira Cupideira","Love Love bear","Love Love Bear","La Romantic Grande",
 }
 
 -- ══════════════════════════════════════════════════════════════
